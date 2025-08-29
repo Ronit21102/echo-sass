6a511984d66ca761403004f05f54f20aa5c0e019
ronit21102-js <60790772+Ronit21102@users.noreply.github.com>
2025-08-29 23:55:47 +0530

    added mutation func

--- apps/Widget/app/page.tsx
+++ apps/Widget/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
+
 import { useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 export default function Page() {
   const users = useQuery(api.users.getMany);
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>app/web</p>
+      <p>app/widget</p>
       <div className="max-w-sm w-full mx-auto">{JSON.stringify(users)}</div>
     </div>
   );
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,9 +1,18 @@
-import { query } from "./_generated/server.js";
+import { mutation, query } from "./_generated/server.js";
 
 export const getMany = query({
   args: {},
   handler: async (ctx) => {
     const users = await ctx.db.query("users").collect();
     return users;
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const userId = await ctx.db.insert("users", {
+      name: "Khaman",
+    });
   },
 });
